a9c166ea4f187c737bac2aef54a01fa2d6d4d911
Colin Barrett <timber@lava.net>
2016-01-22 01:45:10 -0500

    vim_git_import: fix scoping issue

--- fish/functions/vim_git_import.fish
+++ fish/functions/vim_git_import.fish
@@ -2,6 +2,7 @@
 # Takes the path to the directory where the plugins are, usually ~/.vim/bundle
 # Can be passed a blacklist by adding "--except NAME..." to the arguments. Useful for segregating plugins into different directories
 function vim_git_import --description 'Imports ~/.vim/bundle plugins to git subtrees' --argument from
+    set -l exceptions
     if test (count $argv) -eq 0     
         echo "usage: vim_git_import FROM [--except NAME...]"
         return 1
@@ -10,7 +11,7 @@ function vim_git_import --description 'Imports ~/.vim/bundle plugins to git subt
             echo "usage: vim_git_import FROM [--except NAME...]"
             return 1
         end
-        set -l exceptions $argv[3..(count $argv)]
+        set exceptions $argv[3..(count $argv)]
     end
     for plugin in (find $from -maxdepth 1 | tail -n +2)
         set -l plugin_name (basename $plugin)
@@ -20,8 +21,8 @@ function vim_git_import --description 'Imports ~/.vim/bundle plugins to git subt
         set -l prefix (git rev-parse --show-prefix)$plugin_name
         set -l url (pushd $plugin; git remote -v | head -n 1 | cut -f 2 | cut -d ' ' -f 1; popd)
         pushd (git rev-parse --show-toplevel)
-            git subtree add --prefix $prefix --squash $url master
-            read _
+            # echo  "git subtree add --prefix $prefix --squash $url master"
+            git subtree add --prefix $prefix --squash $url master; read _
         popd
     end
 end
