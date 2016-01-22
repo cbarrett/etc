e019c1dd69b472abf9f91198b8d989210347bcfb
Colin Barrett <colin@springsandstruts.com>
2016-01-22 14:45:17 -0500

    Add Hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,32 @@
+-- Open Chrome conditionally
+function hs.urlevent.httpCallback (_, _, _, fullUrl)
+  -- TODO make this conditional on being on work machine
+  local isSlack = frontmost and frontmost:bundleID() == "com.tinyspeck.slackmacgap" 
+  local cmdDown = hs.eventtap.checkKeyboardModifiers()["cmd"] 
+  if cmdDown or isSlack then
+    hs.urlevent.openURLWithBundle(fullUrl, "com.google.Chrome")
+  else
+    hs.urlevent.openURLWithBundle(fullUrl, "com.apple.Safari") 
+  end
+end
+
+-- Track the frontmost app that is NOT Hammerspoon
+-- (Sometimes Slack will be de-activated before httpCallback above runs)
+frontmost = nil
+hs.application.watcher.new(function(_, event, app)
+  if (event == hs.application.watcher.activated
+      and app:bundleID() ~= "org.hammerspoon.Hammerspoon") then
+    frontmost = app
+  end
+end):start()
+
+-- Paste from the search pasteboard
+hs.hotkey.bind({"cmd", "alt"}, "v", nil, function()
+  local old = hs.pasteboard.getContents()
+  local find = hs.pasteboard.getContents("Apple CFPasteboard find")
+  hs.pasteboard.setContents(find)
+  hs.eventtap.keyStroke({"cmd"}, "v")
+  hs.timer.doAfter(0.1, function()
+    hs.pasteboard.setContents(old)
+  end)
+end) 
