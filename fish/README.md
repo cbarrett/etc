c335a9710e2c5690e980f81f2b912323e9af7412
Colin Barrett <colin@springsandstruts.com>
2016-01-22 01:21:58 -0500

    Add first fish function, used when importing these bundles

---
+++ fish/README.md
@@ -0,0 +1,2 @@
+## Usage
+Add the `functions` directory to `$fish_function_path`. It's just that easy!
---
+++ fish/functions/vim_git_import.fish
@@ -0,0 +1,27 @@
+# Run this in the directory you want the subtrees to be located in
+# Takes the path to the directory where the plugins are, usually ~/.vim/bundle
+# Can be passed a blacklist by adding "--except NAME..." to the arguments. Useful for segregating plugins into different directories
+function vim_git_import --description 'Imports ~/.vim/bundle plugins to git subtrees' --argument from
+    if test (count $argv) -eq 0     
+        echo "usage: vim_git_import FROM [--except NAME...]"
+        return 1
+    else if test (count $argv) -ge 2
+        if test $argv[2] != "--except"
+            echo "usage: vim_git_import FROM [--except NAME...]"
+            return 1
+        end
+        set -l exceptions $argv[3..(count $argv)]
+    end
+    for plugin in (find $from -maxdepth 1 | tail -n +2)
+        set -l plugin_name (basename $plugin)
+        if contains $plugin_name $exceptions
+            continue
+        end
+        set -l prefix (git rev-parse --show-prefix)$plugin_name
+        set -l url (pushd $plugin; git remote -v | head -n 1 | cut -f 2 | cut -d ' ' -f 1; popd)
+        pushd (git rev-parse --show-toplevel)
+            git subtree add --prefix $prefix --squash $url master
+            read _
+        popd
+    end
+end
