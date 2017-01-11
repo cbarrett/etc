4cef890877356ed77534be9ec5004b569e5faa66
Colin Barrett <colin@springsandstruts.com>
2017-01-11 15:41:55 -0500

    Finally get cmd-backslach mode working for pasting; other cleanup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,25 @@
+--| Modes
+
+-- TODO add timeouts
+
+function submode(m, mods, key)
+  local n = hs.hotkey.modal.new(nil, nil, nil)
+  function n:exited()
+    m:exit()
+  end
+  m:bind(mode, key, function () n:enter() end, nil, nil)
+  return n
+end
+
+slash = hs.hotkey.modal.new({"cmd"}, "\\", nil)
+p = submode(slash, nil, "p")
+p:bind(nil, "f", function () pf() p:exit() end, nil, nil)
+p:bind(nil, "s", function () ps() p:exit() end, nil, nil)
+
+--| Open links from Slack in Chrome
+
 -- Open Chrome conditionally
 function hs.urlevent.httpCallback (_, _, _, fullUrl)
-  -- TODO make this conditional on being on work machine
   local isSlack = frontmost and frontmost:bundleID() == "com.tinyspeck.slackmacgap" 
   local cmdDown = hs.eventtap.checkKeyboardModifiers()["cmd"] 
   if cmdDown or isSlack then
@@ -13,25 +32,30 @@ end
 -- Track the frontmost app that is NOT Hammerspoon
 -- (Sometimes Slack will be de-activated before httpCallback above runs)
 frontmost = nil
-hs.application.watcher.new(function(_, event, app)
+w = hs.application.watcher.new(function(_, event, app)
   if (event == hs.application.watcher.activated
       and app:bundleID() ~= "org.hammerspoon.Hammerspoon") then
     frontmost = app
   end
-end):start()
-
--- Paste from the find pasteboard
-hs.hotkey.bind({"cmd", "alt"}, "v", nil, function()
-  local old = hs.pasteboard.getContents()
-  pf()
-  hs.eventtap.keyStroke({"cmd"}, "v")
-  hs.timer.doAfter(0.1, function()
-    hs.pasteboard.setContents(old)
-  end)
 end)
+w:start()
 
--- Puts the find pasteboard on the regular pasteboard
-function pf()
+--| Pasting
+
+-- Swap the find pasteboard and the regular pasteboard
+function ps()
+  local pb = hs.pasteboard.getContents()
   local find = hs.pasteboard.getContents("Apple CFPasteboard find")
   hs.pasteboard.setContents(find)
+  hs.pasteboard.setContents(pb, "Apple CFPasteboard find")
 end
+
+-- Paste from the find pasteboard
+function pf()
+  ps()
+  hs.eventtap.keyStroke({"cmd"}, "v")
+  hs.timer.doAfter(0.1, function()
+      ps()
+  end)
+end
+
