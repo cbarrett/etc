096f623b1f29ae5bfc1d4b07db210708b1ddbc9a
Colin Barrett <colin@springsandstruts.com>
2019-03-01 22:15:03 -0500

    Make this slightly better...

--- fish/functions/vim_git_import.fish
+++ fish/functions/vim_git_import.fish
@@ -1,14 +1,14 @@
 # Run this in the directory you want the subtrees to be located in
 # Takes the name of the plugin's directory 
-function vim_git_import --description 'Imports ~/.vim/bundle plugins to git subtrees' --argument plugin
-    if test (count $argv) -ne 1
-        echo "usage: vim_git_import PLUGIN"
-        return 1
-    end
-    set -l prefix (git rev-parse --show-prefix)$plugin
-    set -l url (pushd $plugin; git remote -v | head -n 1 | cut -f 2 | cut -d ' ' -f 1; popd)
-    pushd (git rev-parse --show-toplevel)
-    # echo  "git subtree add --prefix $prefix --squash $url master"
-    git subtree add --prefix $prefix --squash $url master
-    popd
+function vim_git_import --description 'Imports vim plugins to git subtrees' --argument plugin
+  if test (count $argv) -ne 1
+    echo "usage: vim_git_import PLUGIN"
+    return 1
+  end
+  set -l prefix (git rev-parse --show-prefix)$plugin
+  set -l url (pushd $plugin; git remote -v | head -n 1 | cut -f 2 | cut -d ' ' -f 1; popd)
+  pushd (git rev-parse --show-toplevel)
+  echo  "git subtree add --prefix $prefix --squash $url master"
+  # git subtree add --prefix $prefix --squash $url master
+  popd
 end
