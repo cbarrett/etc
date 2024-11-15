9a3a4c7a34dd83a06c490dd7839db0f40d9d35b0
Colin Barrett <colin@springsandstruts.com>
2024-11-15 13:08:37 -1000

    nix-darwin updates:
    - Current package list
    - Remnants of failed flakification
    - system.stateVersion 5

--- nix/darwin-configuration.nix
+++ nix/darwin-configuration.nix
@@ -1,19 +1,52 @@
-{ config, lib, options, pkgs, ... }:
+{ config, lib, options, pkgs, /* inputs, */ ... }:
+let unstable =
+  import <nixpkgs-unstable> { config = config.nixpkgs.config; };
+in
 {
+  # not needed with flakes
   environment.darwinConfig = "/Users/cbarrett/.local/etc/nix/darwin-configuration.nix";
   environment.systemPackages =
     with pkgs;
     [
+      (agda.withPackages (p: [
+        p.cubical
+        p.standard-library
+        # Warning: HoTTest Summer School needs stdlib-1.3
+/*
+        (p.mkDerivation {
+          pname = "plfa";
+          version = "1.0.0";
+          src = /Users/cbarrett/Documents/Code/plfa.github.io/src/plfa.agda-lib;
+          meta.description = "Programming Language Foundations in Agda";
+         })
+*/
+      ]))
+      unstable.bun
+      cmake
+      doctl
+      (haskellPackages.ghcWithPackages (p: [p.tidal]))
+      inkscape
       ispell
-      nodejs
-      mercurial
+      imgcat
+      # nodejs
+      # ocamlPackages.dune_3
+      # ocamlPackages.merlin
+      # ocamlPackages.ocaml
+      # ocamlPackages.ocp-indent
+      # ocamlPackages.utop
+      # ocamlformat
       ripgrep
+      streamlink
       xz
-      yarn
+      update-nix-fetchgit
+      # unstable.yarn-berry
     ];
+  environment.variables.EDITOR = "/usr/bin/vim";
   environment.variables.NIX_REMOTE = "daemon";
 
-  programs.bash.enable = true;
+  ids.uids.nixbld = 300;
+
+  programs.zsh.enable = true;
   programs.fish.enable = true;
   programs.fish.interactiveShellInit = ''
     set fish_function_path /Users/cbarrett/.local/etc/fish/functions $fish_function_path
@@ -26,10 +59,16 @@
   /* services.postgresql.enable = true; */
   /* services.redis.enable = true; */
 
-  system.stateVersion = 4;
+  # system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+  system.stateVersion = 5;
 
-  /* nix.nixPath = [ { darwin = "/Users/cbarrett/Documents/Code/nix-darwin/default.nix"; } ]; */
+  # not needed with flakes
+  /* nix.nixPath = [ "darwin=/Users/cbarrett/Documents/Code/nix-darwin/default.nix" ]; */
   nix.package = pkgs.nix;
   nix.settings.max-jobs = 2;
   nix.settings.trusted-users = [ "root" "cbarrett" ];
+  nix.settings.experimental-features = "nix-command";
+
+  nixpkgs.hostPlatform = "x86_64-darwin";
+
 }
