34339400ab2168159c9ac685e611a8dc1ec97a9d
Colin Barrett <colin@springsandstruts.com>
2024-11-15 14:01:07 -1000

    Bump down my font size, it's like getting a bigger monitor

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -281,7 +281,7 @@ It should only modify the values of Spacemacs settings."
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
    dotspacemacs-default-font '("Base Mono Narrow OT"
-                               :size 16.0
+                               :size 14.0
                                :weight normal
                                :width normal)
 
